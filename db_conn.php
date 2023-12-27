fd24e403dddf52ee9c982b472da1a170a8a69d5f
BryzMcln <jhnbrypntjmcln@gmail.com>
2023-12-27 20:53:58 +0800

    somwthing in progress

--- db_conn.php
+++ db_conn.php
@@ -4,12 +4,11 @@ $username = "root";
 $password = "root_macalinao50";
 $dbase = "mydb"; 
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbase);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    //echo "Connected successfully!";
 }
-//echo "connected.";
 ?>
