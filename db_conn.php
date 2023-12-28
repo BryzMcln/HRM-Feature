b992f5a989e6d90baa5ceca7c917c3d4398fee86
BryzMcln <jhnbrypntjmcln@gmail.com>
2023-12-28 11:36:37 +0800

    db connect change and home css

--- db_conn.php
+++ db_conn.php
@@ -2,13 +2,14 @@
 $servername = "localhost";
 $username = "root";
 $password = "root_macalinao50";
-$dbase = "mydb"; 
+$dbname = "mydbphp";
 
-$conn = new mysqli($servername, $username, $password, $dbase);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
     //echo "Connected successfully!";
 }
-?>
